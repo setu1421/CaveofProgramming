7e11c3d776bd304e43e0c96d8487db82e2484473
setu <setu.1421@yahoo.com>
2015-11-14 19:18:54 +0600

    Starting Basic Thread

---
+++ Thread1/src/demo3/App.java
@@ -0,0 +1,23 @@
+package demo3;
+
+public class App {
+
+	public static void main(String[] args) {
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					System.out.println("Hello:" + i);
+				}
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		});
+		t.start();
+
+	}
+
+}
